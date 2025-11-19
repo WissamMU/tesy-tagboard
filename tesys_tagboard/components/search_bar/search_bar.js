5843edf82f757e3c67ec311ab2e445c4bd49651b
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2025-11-18 22:37:03 -0500

    feat: add search bar autocomplete navigation with up/down arrows
    Also enables 'Enter' to submit selection

--- tesys_tagboard/components/search_bar/search_bar.js
+++ tesys_tagboard/components/search_bar/search_bar.js
@@ -1,24 +1,86 @@
 (function () { // Self invoking function to avoid variable clashing
-  let root = document.querySelector(".search-container");
-  let search_input = root.querySelector("input[type='search']")
-  function get_search_results() {
-    return root.querySelector(".result-container ul");
+  const root = document.querySelector(".search-container");
+  const search_input = root.querySelector("input[type='search']");
+  const result_container = root.querySelector(".result-container");
+  let autocomplete_items = [];
+  let active_autocomplete_index = -1;
+
+  const reset_active_autocomplete_index = () => { active_autocomplete_index = -1 };
+
+  const get_search_results = () => {
+    return result_container.querySelector("ul");
   }
 
-  function remove_results() {
-    let search_results = get_search_results();
-    if (search_results) {
-      htmx.remove(search_results)
+  const get_search_result_items = () => {
+    try {
+      return get_search_results().querySelectorAll("li");
+    } catch (TypeError) {
+      return null
     }
   }
 
-  htmx.on(search_input, "blur", (e) => {
-    // TODO: refactor to handle result selection with arrow keys or TAB
-    if (!e.currentTarget.contains(e.relatedTarget)) {
-      remove_results();
+  let remove_results = () => {
+    let search_results_ul = get_search_results();
+    if (search_results_ul) {
+      htmx.remove(search_results_ul)
+    }
+    reset_active_autocomplete_index()
+  }
+
+  function focus_autocomplete_item(index) {
+    let items = get_search_result_items()
+    if (items) {
+      items = Array.from(items);
+      if (index >= 0 && index < items.length) {
+        items[index].focus();
+        active_autocomplete_index = index
+      }
+    }
+  }
+
+  document.addEventListener("keydown", (e) => {
+    if (root.contains(document.activeElement)) {
+      switch (e.code) {
+        case "ArrowDown":
+          e.preventDefault();
+          focus_autocomplete_item(active_autocomplete_index + 1);
+          break;
+        case "ArrowUp":
+          e.preventDefault();
+          focus_autocomplete_item(active_autocomplete_index - 1);
+          break;
+        case "Enter":
+          // Implemented in encapsulating component (e.g. add_tagset.html)
+          e.preventDefault();
+        default:
+      }
     }
   });
 
 
+  htmx.on(search_input, "blur", (e) => {
+    // TODO: refactor to handle result selection with arrow keys or TAB
+    if (!result_container.contains(e.relatedTarget)) {
+      remove_results();
+    }
+  });
 
+  htmx.on(result_container, "htmx:afterSwap", (e) => {
+    reset_active_autocomplete_index();
+  });
+
+  htmx.on(search_input, "change", (e) => {
+    result_container.querySelectorAll("li").forEach(li => {
+      htmx.on(li, "focus", (e) => {
+        // Focus correct autocomplete item
+
+      });
+
+      htmx.on(li, "blur", (e) => {
+        if (!result_container.contains(e.relatedTarget)) {
+          remove_results();
+        }
+      });
+    });
+  });
 })();
