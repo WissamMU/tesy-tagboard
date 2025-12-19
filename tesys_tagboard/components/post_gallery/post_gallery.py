4e5100fd9019f923bccb8d74af4a6e3bdbe0c70d
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2025-12-18 19:38:32 -0500

    fix: server error when loading post_gallery for unauthenticated users

--- tesys_tagboard/components/post_gallery/post_gallery.py
+++ tesys_tagboard/components/post_gallery/post_gallery.py
@@ -13,12 +13,15 @@ class PostGalleryComponent(Component):
 
     def get_template_data(self, args, kwargs, slots, context):
         posts: PostQuerySet = kwargs.get("posts")
-        collections: CollectionQuerySet = kwargs.get("collections")
-        if collections:
-            collections = collections.for_user(self.request.user).with_gallery_data()
-        else:
-            collections = Collection.objects.for_user(
-                self.request.user
-            ).with_gallery_data()
+        collections: CollectionQuerySet = kwargs.get("collections", [])
+        if self.request.user.is_authenticated:
+            if collections:
+                collections = collections.for_user(
+                    self.request.user
+                ).with_gallery_data()
+            else:
+                collections = Collection.objects.for_user(
+                    self.request.user
+                ).with_gallery_data()
 
         return {"collections": collections, "posts": posts}
