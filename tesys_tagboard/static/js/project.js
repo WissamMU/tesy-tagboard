540480e26322ef43b3bb05fda94feea183f7f9b5
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2025-12-22 19:50:44 -0500

    refactor: allauth templates
    - Copy in all allauth default templates
    - Restructure to use our base.html for login, signup, and password resets

--- tesys_tagboard/static/js/project.js
+++ tesys_tagboard/static/js/project.js
@@ -1,1 +1,9 @@
 /* Project specific Javascript goes here. */
+const messages = document.querySelectorAll(".django-message");
+
+messages.forEach(m => {
+  const close_btn = m.querySelector(".btn-close");
+  close_btn.addEventListener("click", e => {
+    m.remove();
+  });
+});
