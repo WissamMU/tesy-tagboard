870d48294c9777a4288fc444e02fe25617f2ec3d
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2026-01-23 11:01:04 -0500

    test: add max query tests for main pages
    Also refactor tag_search tests fixture into conftest.py

--- tesys_tagboard/conftest.py
+++ tesys_tagboard/conftest.py
@@ -2,6 +2,9 @@ from typing import TYPE_CHECKING
 
 import pytest
 
+from tesys_tagboard.enums import TagCategory
+from tesys_tagboard.models import Tag
+from tesys_tagboard.models import TagAlias
 from tesys_tagboard.users.tests.factories import UserFactory
 
 if TYPE_CHECKING:
@@ -16,3 +19,77 @@ def _media_storage(settings, tmpdir) -> None:
 @pytest.fixture
 def user(db) -> User:
     return UserFactory()
+
+
+@pytest.fixture(scope="session")
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        Tag.objects.bulk_create(
+            [
+                Tag(name="alabaster", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="amaranth pink", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="amaranth purple", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="amaranth", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="amber", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="arctic white", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="beige", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="black", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="blue jeans", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="blue", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="blue-gray", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="blueberry", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="brown", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="crimson", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="evergreen", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="green", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="grey", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="indigo", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="lime green", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="orange", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="purple", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="red", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="red vs. blue", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="sky blue", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="violet", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="violet hyacinth", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="white", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="white rapids", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="yellow", category=TagCategory.BASIC.value.shortcode),
+                Tag(name="yellow flowers", category=TagCategory.BASIC.value.shortcode),
+                Tag(
+                    name="Avery Luis Stein", category=TagCategory.ARTIST.value.shortcode
+                ),
+                Tag(name="Elif", category=TagCategory.ARTIST.value.shortcode),
+                Tag(name="Garbold Loop", category=TagCategory.ARTIST.value.shortcode),
+                Tag(name="Justin Knope", category=TagCategory.ARTIST.value.shortcode),
+                Tag(name="Solomon Steven", category=TagCategory.ARTIST.value.shortcode),
+                Tag(name="Terry Toller", category=TagCategory.ARTIST.value.shortcode),
+                Tag(name="Warren Witt", category=TagCategory.ARTIST.value.shortcode),
+                Tag(name="Yura Yebolsky", category=TagCategory.ARTIST.value.shortcode),
+                Tag(name="Zammy Zolan", category=TagCategory.ARTIST.value.shortcode),
+                Tag(name="CC-BY", category=TagCategory.COPYRIGHT.value.shortcode),
+                Tag(name="MIT", category=TagCategory.COPYRIGHT.value.shortcode),
+                Tag(
+                    name="Public Domain", category=TagCategory.COPYRIGHT.value.shortcode
+                ),
+                Tag(name="Unlicense", category=TagCategory.COPYRIGHT.value.shortcode),
+            ]
+        )
+
+        TagAlias.objects.bulk_create(
+            [
+                TagAlias(name="arctic", tag=Tag.objects.get(name="arctic white")),
+                TagAlias(name="gray", tag=Tag.objects.get(name="grey")),
+                TagAlias(name="r v. b", tag=Tag.objects.get(name="red vs. blue")),
+                TagAlias(name="r vs. b", tag=Tag.objects.get(name="red vs. blue")),
+                TagAlias(name="red v. blue", tag=Tag.objects.get(name="red vs. blue")),
+                TagAlias(name="red vs blue", tag=Tag.objects.get(name="red vs. blue")),
+                TagAlias(name="red x blue", tag=Tag.objects.get(name="red vs. blue")),
+                TagAlias(name="blue-berry", tag=Tag.objects.get(name="blueberry")),
+                TagAlias(name="gray-blue", tag=Tag.objects.get(name="blue-gray")),
+                TagAlias(name="bluejeans", tag=Tag.objects.get(name="blue jeans")),
+                TagAlias(name="Justin K", tag=Tag.objects.get(name="Justin Knope")),
+                TagAlias(name="Solomon S", tag=Tag.objects.get(name="Solomon Steven")),
+                TagAlias(name="Z. Zolan", tag=Tag.objects.get(name="Zammy Zolan")),
+            ]
+        )
--- tesys_tagboard/tests/test_views.py
+++ tesys_tagboard/tests/test_views.py
@@ -1,56 +1,84 @@
 from http import HTTPStatus
 
-from django.test import Client
-from django.test import TestCase
+import pytest
 from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
+
+from tesys_tagboard.users.models import User
 
 
-class TestHomeView(TestCase):
-    def setUp(self):
-        # Setup run before every test method.
-        pass
-
-    def tearDown(self):
-        # Clean up run after every test method.
-        pass
-
-    def test_home(self):
-        client = Client()
+@pytest.mark.django_db
+class TestHomeView:
+    def test_home(self, client):
         response = client.get(reverse("home"))
         assert response.status_code == HTTPStatus.OK
+        assertTemplateUsed(response, "pages/home.html")
+
+    def test_max_query_count(self, client):
+        client.get(reverse("home"))
 
 
-class TestTagsView(TestCase):
-    def test_tags(self):
-        client = Client()
+@pytest.mark.django_db
+class TestTagsView:
+    def test_tags(self, client):
         response = client.get(reverse("tags"))
         assert response.status_code == HTTPStatus.OK
 
+    def test_max_query_count(self, client, django_assert_max_num_queries):
+        with django_assert_max_num_queries(15):
+            client.get(reverse("home"))
 
-class TestPostsView(TestCase):
-    def test_posts(self):
-        client = Client()
+
+@pytest.mark.django_db
+class TestPostsView:
+    def test_posts(self, client):
         response = client.get(reverse("posts"))
         assert response.status_code == HTTPStatus.OK
+        assertTemplateUsed(response, "pages/posts.html")
+
+    def test_max_query_count(self, client, django_assert_max_num_queries):
+        with django_assert_max_num_queries(20):
+            client.get(reverse("posts"))
+
+    def test_max_logged_in_query_count(self, client, django_assert_max_num_queries):
+        user = User.objects.get(username="user1")
+        client.force_login(user)
+        with django_assert_max_num_queries(25):
+            client.get(reverse("posts"))
 
 
-class TestUploadView(TestCase):
-    def test_upload(self):
-        client = Client()
-        response = client.get("/upload/", follow=True)
+@pytest.mark.django_db
+class TestUploadView:
+    def test_upload(self, client):
+        response = client.get(reverse("upload"), follow=True)
         assert response.redirect_chain[0][0] == "/accounts/login/?next=/upload/"
         assert response.status_code == HTTPStatus.OK
+        assertTemplateUsed(response, "account/login.html")
+
+    def test_max_query_count(self, client, django_assert_max_num_queries):
+        with django_assert_max_num_queries(20):
+            client.get(reverse("upload"))
 
 
-class TestCollectionsView(TestCase):
-    def test_collections(self):
-        client = Client()
+@pytest.mark.django_db
+class TestCollectionsView:
+    def test_collections(self, client):
         response = client.get(reverse("collections"))
         assert response.status_code == HTTPStatus.OK
+        assertTemplateUsed(response, "pages/collections.html")
+
+    def test_max_query_count(self, client, django_assert_max_num_queries):
+        with django_assert_max_num_queries(20):
+            client.get(reverse("collections"))
 
 
-class TestHelpView(TestCase):
-    def test_help(self):
-        client = Client()
+@pytest.mark.django_db
+class TestHelpView:
+    def test_help(self, client):
         response = client.get(reverse("help"))
         assert response.status_code == HTTPStatus.OK
+        assertTemplateUsed(response, "pages/help.html")
+
+    def test_max_query_count(self, client, django_assert_max_num_queries):
+        with django_assert_max_num_queries(20):
+            client.get(reverse("help"))
